5588f0c18623898638fcfef1051ba2341fbe6ea7
张欣宇 <176956856@qq.com>
2017-04-24 17:29:31 +0800

    完善Image操作、Bucket操作。

--- src/Bucket.php
+++ src/Bucket.php
@@ -1,43 +1,160 @@
 <?php
-
 namespace AprSoft\Aliyun\OSS;
 
+use OSS\Core\OssException;
 use OSS\OssClient;
+use Yii;
 use yii\base\Component;
+use yii\log\Logger;
+
+
+/**
+ * Class OssClient
+ *
+ * Object Storage Service(OSS) 的客户端类，封装了用户通过OSS API对OSS服务的各种操作，
+ * 用户通过OssClient实例可以进行 Bucket, ACL等操作，具体
+ * 的接口规则可以参考官方OSS API文档
+ */
 
 class Bucket extends Component
 {
-    protected $ossClient;
 
-    protected $bucket;
+    // OSS获得的AccessKeyId
+    public $accessKeyId;
+    // OSS获得的AccessKeySecret
+    public $accessKeySecret;
+    // OSS获得的bucket
+    public $bucket = null;
+    //OSS数据中心访问域名，例如oss-cn-hangzhou.aliyuncs.com
+    public $endpoint;
+    //是否对Bucket做了域名绑定，并且Endpoint参数填写的是自己的域名
+    public $isCName = false;
 
-    public function __construct($accessKeyId, $accessKeySecret, $endpoint, $isCName = false, $securityToken = null)
-    {
-        $this->ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint, $isCName, $securityToken);
-    }
-    public function create($bucket, $acl = OssClient::OSS_ACL_TYPE_PRIVATE, $options = null)
-    {
-        return $this->ossClient->createBucket($bucket, $acl, $options);
-    }
+    public $securityToken = null;
 
-    public function delete($bucket, $options = null)
-    {
-        return $this->ossClient->deleteBucket($bucket, $acl, $options);
-    }
+    private $_ossClient;
 
-    public function exist($bucket)
+    public function getClient()
     {
-        return $this->ossClient->doesBucketExist($bucket);
-    }
-
-    public function getAcl($bucket, $options = null)
-    {
-        return $this->ossClient->doesBucketExist($bucket);
+        if ($this->_ossClient === null) {
+            $this->setClient(new OssClient($this->accessKeyId, $this->accessKeySecret, $this->endpoint, $this->isCName, $this->securityToken));
+        }
+        return $this->_ossClient;
     }
 
-    public function setAcl($bucket, $acl, $options = null)
+    public function setClient(OssClient $ossClient)
     {
-        return $this->ossClient->putBucketAcl($bucket, $acl, $options);
+        $this->_ossClient = $ossClient;
+    }
+
+    /**
+     * 创建bucket
+     *
+     * @param string $bucket
+     * @param string $acl 默认创建的bucket的ACL是OssClient::OSS_ACL_TYPE_PRIVATE
+     *
+     * @param array $options
+     * @return boolean
+     */
+    public function create(string $bucket, string $acl = OssClient::OSS_ACL_TYPE_PRIVATE, array $options = null)
+    {
+        try {
+            $this->client->createBucket($bucket, $acl, $options);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 删除bucket
+     * 如果Bucket不为空（Bucket中有Object，或者有分块上传的碎片），则Bucket无法删除，
+     * 必须删除Bucket中的所有Object以及碎片后，Bucket才能成功删除。
+     *
+     * @param string $bucket
+     * @param array $options
+     * @return boolean
+     */
+    public function delete(string $bucket, array $options = null)
+    {
+        try {
+            $this->client->deleteBucket($bucket, $options);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 判断bucket是否存在
+     *
+     * @param string $bucket
+     * @return bool
+     */
+    public function exist(string $bucket)
+    {
+        try {
+            $this->client->doesBucketExist($bucket);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 列举用户所有的Bucket[GetService], Endpoint类型为cname不能进行此操作
+     *
+     * @param array $options
+     * @return BucketListInfo
+     */
+    function list(array $options = null) {
+        try {
+            $list = $this->client->listBuckets($options);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return $list->getBucketList();
+    }
+
+    /**
+     * 获取bucket的ACL配置情况
+     *
+     * @param string $bucket
+     * @param array $options
+     * @return string
+     */
+    public function getAcl(string $bucket, array $options = null)
+    {
+        try {
+            $acl = $this->client->getBucketAcl($bucket, $options);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return $acl;
+    }
+
+    /**
+     * 设置bucket的ACL配置情况
+     *
+     * @param string $bucket bucket名称
+     * @param string $acl 读写权限，可选值 ['private', 'public-read', 'public-read-write']
+     * @param array $options 可以为空
+     * @return boolean
+     */
+    public function setAcl(string $bucket, string $acl = OssClient::OSS_ACL_TYPE_PRIVATE, array $options = null)
+    {
+        try {
+            $this->client->putBucketAcl($bucket, $acl, $options);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return true;
     }
 
 }
--- src/Image.php
+++ src/Image.php
@@ -8,7 +8,11 @@ use yii\base\Component;
 use yii\log\Logger;
 
 /**
- * @todo Aliyun 图片上传
+ * Class OssClient
+ *
+ * Object Storage Service(OSS) 的客户端类，封装了用户通过OSS API对OSS服务的各种操作，
+ * 用户通过OssClient实例可以进行Object，MultipartUpload, ACL等操作，具体
+ * 的接口规则可以参考官方OSS API文档
  */
 class Image extends Component
 {
@@ -169,6 +173,67 @@ class Image extends Component
             return false;
         }
         return true;
+    }
+
+    /**
+     * 获取object的ACL属性
+     *
+     * @param string $name
+     * @return string
+     */
+    public function getAcl(string $name)
+    {
+        try {
+            $acl = $this->client->getObjectAcl($this->bucket, $name);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return $acl;
+    }
+
+    /**
+     * 设置object的ACL属性
+     *
+     * @param string $name object名称
+     * @param string $acl 读写权限，可选值 ['default', 'private', 'public-read', 'public-read-write']
+     * @param array $options
+     * @return boolean
+     */
+    public function setAcl(string $name, string $acl = OssClient::OSS_ACL_TYPE_PRIVATE, array $options = null)
+    {
+        try {
+            $this->client->putObjectAcl($this->bucket, $name, $acl, $options);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 获取bucket下的object列表
+     *
+     * @param array $options
+     * 其中options中的参数如下
+     * $options = array(
+     *      'max-keys'  => max-keys用于限定此次返回object的最大数，如果不设定，默认为100，max-keys取值不能大于1000。
+     *      'prefix'    => 限定返回的object key必须以prefix作为前缀。注意使用prefix查询时，返回的key中仍会包含prefix。
+     *      'delimiter' => 是一个用于对Object名字进行分组的字符。所有名字包含指定的前缀且第一次出现delimiter字符之间的object作为一组元素
+     *      'marker'    => 用户设定结果从marker之后按字母排序的第一个开始返回。
+     *)
+     * 其中 prefix，marker用来实现分页显示效果，参数的长度必须小于256字节。
+     * @return ObjectListInfo
+     * */
+    public function list($options)
+    {
+        try {
+            $content = $this->client->listObjects($this->bucket, $options);
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'oss');
+            return false;
+        }
+        return $content;
     }
 
 }
